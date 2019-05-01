d6888bc52ede091d6f0f3f867277914152c83357
nakzz <naqibprio10@gmail.com>
2019-05-01 14:05:24 -0500

    Config file added

---
+++ application/Config.java
@@ -0,0 +1,21 @@
+package application;
+
+public class Config {
+
+  //JSON FILE NAME
+  public static final String JSON_LOCATION = "PROFILE_TYPE_FIELD";
+  
+  //ALL THE FIELD IN JSON file
+  public static final String NAME_FIELD = "PROFILE_TYPE_FIELD";
+  public static final String PROFILE_TYPE_FIELD = "PROFILE_TYPE_FIELD";
+  public static final String IS_ADMIN_FIELD = "PROFILE_TYPE_FIELD";
+  public static final String IS_PUBLIC_FIELD = "PROFILE_TYPE_FIELD";
+  public static final String MAJORS_FIELD = "PROFILE_TYPE_FIELD";
+  public static final String CERTIFICATES_FIELD = "PROFILE_TYPE_FIELD";
+  public static final String SCHOLARSHIPS_FIELD = "PROFILE_TYPE_FIELD";
+  public static final String COURSES_FIELD = "PROFILE_TYPE_FIELD";
+  public static final String WORK_EXPERIANCES_FIELD = "PROFILE_TYPE_FIELD";
+
+  
+  
+}
